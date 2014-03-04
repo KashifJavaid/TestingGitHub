a095629bf6840e26fa2723c7476fdb88335066d2
gustyromio <mr.kashif.samman@gmail.com>
2014-03-04 12:05:52 +0000

    WeShppingKashif

---
+++ WEShopping 2/WEShopping/items.h
@@ -0,0 +1,23 @@
+//
+//  items.h
+//  WEShopping
+//
+//  Created by Unbounded on 2/17/14.
+//  Copyright (c) 2014 Unbounded. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <CoreData/CoreData.h>
+
+@class category;
+
+@interface items : NSManagedObject
+
+@property (nonatomic, retain) NSDate * dateCreated;
+@property (nonatomic, retain) NSDate * dateModified;
+@property (nonatomic, retain) NSString * itemName;
+@property (nonatomic, retain) NSNumber * webid;
+@property (nonatomic, retain) NSDate * webUpdatedTime;
+@property (nonatomic, retain) category *inCategory;
+
+@end
